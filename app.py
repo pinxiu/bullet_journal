b34288054815a9d3081e3df5aad144ffc15b3c6e
Pinxiu Gong <pinxiu.gong@avast.com>
2019-09-11 13:19:46 -0700

    test

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask, request
+from flask.ext.sqlalchemy import SQLAlchemy
+
+# from models import BjRecord
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
 
 
-@app.route('/store/<user_id>', methods = ['POST'])
-def store(user_id):
-	data = request.form
+@app.route('/<user_id>', methods = ['GET', 'POST', 'PUT'])
+def access(user_id):
+	# auth(user_id, request.headers)
+
+	if request.method == 'GET':
+		return #BjRecord.get_record_by_user_id(user_id)
+	elif request.method == 'POST':
+		data = request.get_data()
+		# db.session.add(result)
 	return data
