ff72f511ca526fd5880c4fb0247679ad0ad98cd9
Pinxiu Gong <pinxiu.gong@avast.com>
2019-09-11 13:32:32 -0700

    add config for db url

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['REDIS_URL']
 db = SQLAlchemy(app)
 
 
