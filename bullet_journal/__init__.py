831c29480abab4b66083e72b4663948200eac1b3
Pinxiu Gong <pinxiu.gong@avast.com>
2019-09-16 13:11:12 -0700

    fix init

--- bullet_journal/__init__.py
+++ bullet_journal/__init__.py
@@ -4,6 +4,6 @@ __title__ = "bullet_journal"
 
 __version__ = "0.7"
 
-__prefix__ = f"{os.environ['HOME']}/.bj"
+__prefix__ = os.environ['HOME'] + "/.bj"
 
 __host__ = "https://bjserver.herokuapp.com"
